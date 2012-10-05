e064fde33738376a87a9eb0ef34f1e723e76f276
Sandro Padin <sandropadin@gmail.com>
2012-10-05 16:09:14 -0500

    Latest build.

--- dist/coverphoto.js
+++ dist/coverphoto.js
@@ -4,16 +4,43 @@
 
 this["coverphotoTemplates"] = this["coverphotoTemplates"] || {};
 
+this["coverphotoTemplates"]["src/templates/actions.jst"] = function(obj){
+var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
+with(obj||{}){
+__p+='<div class="actions">\n  <ul class="chooser">\n    <li class="open-menu item"><a href="#change_cover_photo">Change cover photo</a></li>\n    <ul class="menu">\n      <li class="upload item"><a href="#upload_cover_photo">Upload new photo</a></li>\n      <!-- <li class="reposition item"><a href="#reposition_cover_photo">Reposition current photo</a></li> -->\n    </ul>\n  </ul>\n  <ul class="edit">\n    <li class="cancel item"><a href="#cancel">Cancel</a></li>\n    <li class="save item"><a href="#save">Save</a></li>\n  </ul>\n</div>';
+}
+return __p;
+};
+
+this["coverphotoTemplates"]["src/templates/container.jst"] = function(obj){
+var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
+with(obj||{}){
+__p+='<div class="coverphoto-container">\n  <canvas class=\'output\'>\n</div>';
+}
+return __p;
+};
+
 this["coverphotoTemplates"]["src/templates/form.jst"] = function(obj){
 var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
 with(obj||{}){
 __p+='<form action="'+
 ( postUrl )+
-'">\n  <input type="file" name="coverphoto[image]" accepts="image/*">\n</form>';
+'" class="coverphoto-form" method="post" enctype="multipart/form-data">\n  <input type="file" name="coverphoto[original]" accept="image/*">\n  <input type="hidden" name="coverphoto[cropped]">\n</form>';
+}
+return __p;
+};
+
+this["coverphotoTemplates"]["src/templates/image.jst"] = function(obj){
+var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
+with(obj||{}){
+__p+='<div class="coverphoto-photo-container">\n  <img src="'+
+( imageData )+
+'" width="1024">\n</div>';
 }
 return __p;
 };
 (function() {
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
   (function($) {
     var CoverPhoto;
@@ -25,28 +52,72 @@ return __p;
 
       CoverPhoto.defaults = {
         postUrl: '/update_cover_photo',
-        editable: true
+        editable: false
       };
 
       function CoverPhoto(_arg) {
         this.el = _arg.el, this.options = _arg.options;
+        this.handleFileSelected = __bind(this.handleFileSelected, this);
+
+        this.endReposition = __bind(this.endReposition, this);
+
+        this.startReposition = __bind(this.startReposition, this);
+
+        this.cancelEdit = __bind(this.cancelEdit, this);
+
+        this.saveEdit = __bind(this.saveEdit, this);
+
+        this.startUpload = __bind(this.startUpload, this);
+
+        this.hideEditMenu = __bind(this.hideEditMenu, this);
+
+        this.showEditMenu = __bind(this.showEditMenu, this);
+
+        this.hideActionsMenu = __bind(this.hideActionsMenu, this);
+
+        this.showActionsMenu = __bind(this.showActionsMenu, this);
+
+        this.showActions = __bind(this.showActions, this);
+
+        this.hideActions = __bind(this.hideActions, this);
+
+        this.templates = coverphotoTemplates;
         this.setup();
         this.cache();
         this.render();
+        this.elements();
         this.bindEvents();
       }
 
       CoverPhoto.prototype.render = function() {
+        this.addForm();
         if (this.options.editable) {
-          return this.addEditButton();
+          this.addActions();
         }
+        if (this.options.currentImage) {
+          this.addImage(this.options.currentImage);
+        }
+        $("canvas", this.$el).attr("width", this.$el.width());
+        return $("canvas", this.$el).attr("height", this.$el.height());
       };
 
-      CoverPhoto.prototype.bindEvents = function() {};
+      CoverPhoto.prototype.bindEvents = function() {
+        $(this.$el).on("mouseleave", this.hideActions);
+        $(this.$el).on("mouseenter", this.showActions);
+        $(this.$el).on("mouseleave", this.actionsContainer.selector, this.hideActionsMenu);
+        $(this.$el).on("change", this.fileInput.selector, this.handleFileSelected);
+        $(this.$el).on("click", this.openMenuButton.selector, this.showActionsMenu);
+        $(this.$el).on("click", this.uploadButton.selector, this.startUpload);
+        $(this.$el).on("click", this.repositionButton.selector, this.startReposition);
+        $(this.$el).on("click", this.saveEditButton.selector, this.saveEdit);
+        return $(this.$el).on("click", this.cancelEditButton.selector, this.cancelEdit);
+      };
 
       CoverPhoto.prototype.setup = function() {
+        var html;
         this.options = $.extend(CoverPhoto.defaults, this.options);
-        return this.$el = $(this.el);
+        html = this.templates["src/templates/container.jst"]();
+        return this.$el = $(html).appendTo($(this.el));
       };
 
       CoverPhoto.prototype.cache = function() {
@@ -55,8 +126,144 @@ return __p;
         return CoverPhoto.cacheCount++;
       };
 
-      CoverPhoto.prototype.addEditButton = function() {
-        return this.$el.append("<a href=\"#\" class=\"edit\">Change cover photo</a>");
+      CoverPhoto.prototype.elements = function() {
+        this.actionsContainer = $(".actions", this.$el);
+        this.actions = $(".chooser", this.$el);
+        this.actionsMenu = $(".chooser .menu", this.$el);
+        this.editMenu = $(".edit", this.$el);
+        this.cancelEditButton = $(".edit .cancel a", this.$el);
+        this.saveEditButton = $(".edit .save a", this.$el);
+        this.openMenuButton = $(".chooser .open-menu a", this.$el);
+        this.uploadButton = $(".chooser .upload a", this.$el);
+        this.repositionButton = $(".chooser .reposition a", this.$el);
+        this.form = $("form", this.$el);
+        this.fileInput = $("input[name='coverphoto[original]']", this.$el);
+        this.hiddenImageInput = $("input[name='coverphoto[cropped]']", this.$el);
+        return this.canvas = $("canvas", this.$el);
+      };
+
+      CoverPhoto.prototype.addForm = function() {
+        return this.$el.append(this.templates["src/templates/form.jst"](this.options));
+      };
+
+      CoverPhoto.prototype.addActions = function() {
+        return this.$el.append(this.templates["src/templates/actions.jst"](this.options));
+      };
+
+      CoverPhoto.prototype.addImage = function(imageData) {
+        this.originalImage = $(".coverphoto-photo-container img", this.$el).attr("src");
+        $(".coverphoto-photo-container", this.$el).remove();
+        return this.$el.append(this.templates["src/templates/image.jst"]({
+          imageData: imageData
+        }));
+      };
+
+      CoverPhoto.prototype.hideActions = function() {
+        return this.actions.fadeOut();
+      };
+
+      CoverPhoto.prototype.showActions = function() {
+        if (!this.editMenu.is(":visible")) {
+          return this.actions.fadeIn();
+        }
+      };
+
+      CoverPhoto.prototype.showActionsMenu = function() {
+        this.actionsMenu.show();
+        return false;
+      };
+
+      CoverPhoto.prototype.hideActionsMenu = function(evt) {
+        return this.actionsMenu.hide();
+      };
+
+      CoverPhoto.prototype.showEditMenu = function() {
+        this.editMenu.show();
+        return this.actions.hide();
+      };
+
+      CoverPhoto.prototype.hideEditMenu = function() {
+        this.editMenu.hide();
+        return this.actions.show();
+      };
+
+      CoverPhoto.prototype.startUpload = function() {
+        this.fileInput.click();
+        return false;
+      };
+
+      CoverPhoto.prototype.saveEdit = function() {
+        console.log("Saving to " + this.options.postUrl);
+        this.gatherImageData();
+        this.form.submit();
+        this.hideEditMenu();
+        this.endReposition();
+        return false;
+      };
+
+      CoverPhoto.prototype.cancelEdit = function() {
+        if (this.originalImage) {
+          this.addImage(this.originalImage);
+        }
+        this.hideEditMenu();
+        this.endReposition();
+        return false;
+      };
+
+      CoverPhoto.prototype.startReposition = function(evt) {
+        var image, makeImageDraggable,
+          _this = this;
+        if (evt == null) {
+          evt = null;
+        }
+        image = $(".coverphoto-photo-container img", this.$el);
+        makeImageDraggable = function() {
+          var yMax;
+          yMax = -(image.height() - image.parent().height() - 10);
+          return image.draggable({
+            axis: "y",
+            containment: [0, yMax, 0, 0]
+          });
+        };
+        if (image.height() > 0) {
+          makeImageDraggable();
+        } else {
+          image.load(makeImageDraggable);
+        }
+        this.showEditMenu();
+        if (evt != null) {
+          this.hideActionsMenu();
+        }
+        return false;
+      };
+
+      CoverPhoto.prototype.endReposition = function() {
+        var image;
+        image = $(".coverphoto-photo-container img", this.$el);
+        return image.draggable("destroy");
+      };
+
+      CoverPhoto.prototype.handleFileSelected = function(evt) {
+        var file, reader,
+          _this = this;
+        file = evt.target.files[0];
+        reader = new FileReader();
+        reader.onload = function(evt) {
+          _this.addImage(evt.target.result);
+          return _this.startReposition();
+        };
+        return reader.readAsDataURL(file);
+      };
+
+      CoverPhoto.prototype.gatherImageData = function() {
+        var context, dataUrl, height, image, width;
+        image = $(".coverphoto-photo-container img", this.$el);
+        context = this.canvas[0].getContext("2d");
+        width = image.width();
+        height = image.height();
+        context.drawImage(image[0], 0, image.position().top, width, height);
+        dataUrl = this.canvas[0].toDataURL("image/png");
+        return this.hiddenImageInput.val(dataUrl);
       };
 
       return CoverPhoto;
